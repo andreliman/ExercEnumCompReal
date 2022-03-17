dff13ecd60c216b4764a8142c51b65dd77e76ab6
andreliman <andredelimanunes@gmail.com>
2022-03-17 20:37:57 -0300

    Add project files.

---
+++ ExercEnumCompReal/Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ExercEnumCompReal.Entities
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name}, ({BirthDate.ToString("dd/MM/yyyy")}) - {Email}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercEnumCompReal/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercEnumCompReal.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
